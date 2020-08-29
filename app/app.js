6aa05c8f2c3992b0948f6d3e184bd6448d059b7f
James Leffler <jsleffler@gmail.com>
2020-08-29 17:41:28 +0100

    added helmet middleware

--- app/app.js
+++ app/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+const helmet = require('helmet');
+
+// Add middleware and configuration here
+app.use(helmet());
+
 module.exports = app;
