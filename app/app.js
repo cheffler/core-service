ec60fb43f97cbec9945824439758bd7482766859
James Leffler <jsleffler@gmail.com>
2020-08-29 17:53:27 +0100

    added compression

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ const app = express();
 // Middleware
 const helmet = require('helmet');
 const morgan = require('morgan');
+const compression = require('compression');
 
 // Add middleware and configuration here
 app.use(helmet());
 app.use(morgan('combined'));
+app.use(compression());
 
 module.exports = app;
