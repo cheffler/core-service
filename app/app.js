13f503c5f091a17da59a08ed93d226d64b0d4ebb
James Leffler <jsleffler@gmail.com>
2020-08-29 17:43:42 +0100

    added access logs

--- app/app.js
+++ app/app.js
@@ -3,8 +3,10 @@ const app = express();
 
 // Middleware
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 // Add middleware and configuration here
 app.use(helmet());
+app.use(morgan('combined'));
 
 module.exports = app;
