507106b8e51a7cf599401bfd5766d7102f85cfb5
James Leffler <jsleffler@gmail.com>
2020-08-31 13:21:23 +0100

    converted to an https server

--- app/index.js
+++ app/index.js
@@ -1,4 +1,11 @@
+// Built in modules
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+// NPM installed modules
 const express = require('express');
+
 const app = express();
 const port = 2000;
 
@@ -6,6 +13,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Define the path to secrets
+const secretsDir = path.join(__dirname, '..', 'secrets');
+
+// HTTPs options
+const options = {
+  cert: fs.readFileSync(secretsDir + '/coreservice.dev.pem'),
+  key: fs.readFileSync(secretsDir + '/coreservice.dev-key.pem'),
+};
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`Example app listening at https://*.coreservice.dev:${port}`);
 });
